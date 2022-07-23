fb6433dcdb5f43898153c70a0066d7770cf0cb41
Viacheslav Galaktionov <slavka@slavkin.site>
2022-07-23 20:48:08 +0400

    Add backend selection macros

--- source/swe_prefix.h
+++ source/swe_prefix.h
@@ -13,6 +13,23 @@
 ///[OLC_HM] END STD_INCLUDES
 
 ///[OLC_HM] START COMPILER_SWITCHES
-// TODO: Compiler/System Sensitivity
-#define SOUNDWAVE_USING_WINMM
+// Compiler/System Sensitivity
+#if !defined(SOUNDWAVE_USING_WINMM) && !defined(SOUNDWAVE_USING_WASAPI) &&  \
+    !defined(SOUNDWAVE_USING_XAUDIO) && !defined(SOUNDWAVE_USING_OPENAL) && \
+    !defined(SOUNDWAVE_USING_ALSA) && !defined(SOUNDWAVE_USING_SDLMIXER)    \
+
+	#if defined(_WIN32)
+		#define SOUNDWAVE_USING_WINMM
+	#endif
+	#if defined(__linux__)
+		#define SOUNDWAVE_USING_ALSA
+	#endif
+	#if defined(__APPLE__)
+		#define SOUNDWAVE_USING_SDLMIXER
+	#endif
+	#if defined(__EMSCRIPTEN__)
+		#define SOUNDWAVE_USING_SDLMIXER
+	#endif
+
+#endif
 ///[OLC_HM] END COMPILER_SWITCHES

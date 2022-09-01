8f6fc0b6c50eab611057ba8d14ee20ad3f5d3aba
dandistine <34634876+dandistine@users.noreply.github.com>
2022-08-31 20:02:28 -0500

    Update swe_synth_delay.h

--- source/swe_synth_delay.h
+++ source/swe_synth_delay.h
@@ -13,10 +13,13 @@ namespace olc::sound::synth
 			olc::sound::synth::Property input = 0.0;
 			olc::sound::synth::Property output = 0.0;
 			olc::sound::synth::Property decay = 1.0;
-			olc::sound::synth::Property delay = max_ms / 1000.0;
+			olc::sound::synth::Property delay = 1.0;
 			std::array<double, (max_ms * samplerate) / 1000> state{0.0};
-			
 			virtual void Update(uint32_t nChannel, double dTime, double dTimeStep) override;
+		private:
+			double max_delay = static_cast<double>(max_ms) / 1000.0;
+			size_t input_index = 0;
+			size_t output_index = 1;
 		};
 	}
 	///[OLC_HM] END SYNTH_DELAY_H
